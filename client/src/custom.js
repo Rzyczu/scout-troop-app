4c3b03c60fc1fae6ca2e799ea3e26cce98707d88
Rzyczu <70780585+Rzyczu@users.noreply.github.com>
2024-11-17 02:29:57 +0100

    logout fix

--- client/src/custom.js
+++ client/src/custom.js
@@ -5,3 +5,11 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Bootstrap JS initialized');
 });
+
+function logout() {
+    document.cookie = "token=; Max-Age=0; path=/;"; // Clear the token cookie
+    window.location.href = '/login'; // Redirect to login page
+}
+
+// Attach logout function to the global `window` object
+window.logout = logout;
