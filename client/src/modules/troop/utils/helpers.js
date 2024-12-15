541273ea29a13a0d00bf42acf915452c53c0d50c
Rzyczu <70780585+Rzyczu@users.noreply.github.com>
2024-12-15 14:02:00 +0100

    yes

--- client/src/modules/troop/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-export const updateModalTitle = (modal, title) => {
-    const modalTitle = modal.querySelector('.modal-title');
-    if (modalTitle) modalTitle.textContent = title;
-};
-
-export const populateTroopForm = (form, troop) => {
-    form.querySelector('#troopNameInput').value = troop.name || '';
-    form.querySelector('#troopDescriptionInput').value = troop.description || '';
-};
